532083ebbf2b532de2725e80612dffbefd390b3a
kyungwon <kyungwon0928@daum.net>
2022-08-27 17:35:03 +0900

    app 수정

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          효원아 잘하자~^^
-        </a>
-      </header>
-    </div>
-=======
+
     <>
       <Router>
         <Navbar />
@@ -31,7 +14,7 @@ function App() {
         </Routes>
       </Router>
     </>
->>>>>>> 2a742eb2f71d4e2664b554992c94c8a6b290391d
+
   );
 }
 
