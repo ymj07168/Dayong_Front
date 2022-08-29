70587ed035406cff96839fb3af01bb3018d035dd
이효원 <ymj07168@gmail.com>
2022-08-29 13:07:02 +0900

    homepage

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
+import Home from "./routes/Home.js";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import Main from './routes/Main';
-import Loginpage from './routes/Login';
-import Joinpage from './routes/Join';
-import Menu from './routes/Menu';
-import Option from './routes/Option.js';
-import Orderdeliver from "./routes/Orderdeliver";
-import Orderpackage from "./routes/Orderpackage"
-import Basket from './routes/Basket.js'
-import MyPage from './routes/MyPage';
-import MapInfo from './routes/MapInfo';
-import OrderDetail from './routes/OrderDetail';
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
-
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
 
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/main' element={<Main />}/>
-          <Route path='/login' element={<Loginpage />}/>
-          <Route path='/join' element={<Joinpage />}/>
-          <Route path='/menu' element={<Menu />}/>
-          <Route path='/option/:menuname' element={<Option />}/>
-          <Route path='/basket' element={<Basket />}/>
-          <Route path='/orderdelivery' element={<Orderdeliver />}/>
-          <Route path='/orderpackage' element={<Orderpackage />}/>
-          <Route path='/mypage' element={<MyPage />} />
-          <Route path='/mapinfo' element={<MapInfo />} />
-          <Route path='/order-detail' element={<OrderDetail />} />
-
-        </Routes>
-      </Router>
-    </>
   );
 }
 
